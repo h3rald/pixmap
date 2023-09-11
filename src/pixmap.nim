e6e5a7638e6d65232494900fdc1f7ad32fb81e62
h3rald <h3rald@h3rald.com>
2023-09-11 08:25:42 +0200

    Implemented command arguments and options.

--- src/pixmap.nim
+++ src/pixmap.nim
@@ -1,22 +1,26 @@
 import 
   std/sequtils,
   std/strutils,
-  nimPNG
+  std/parseopt,
+  std/paths,
+  std/os,
+  nimPNG,
+  pixmappkg/config
 
 type
-  PixMapData = object
+  PixMapData* = object
     pixels: seq[string]
     width: int
     height: int
 
 type
-  PngData = object
+  PngData* = object
     pixels: seq[byte]
     width: int
     height: int
 
 type 
-  PixMap = object
+  PixMap* = object
     width: int
     height: int
     pixels: seq[string]
@@ -91,6 +95,84 @@ proc write*(pixmap: PixMap, outMap: string, outPalette: string) =
   writeFile(outMap, map)
   writeFile(outPalette, palette)
 
+#########################################################################################
+
+let USAGE* = """$1 v$2 - $3
+(c) 2023 $4
+
+Usage:
+  pixmap <source.pixmap> <palette.pixpal> [<options>] Transforms <source.pixmap> into a PNG image 
+                          using <palette.pixpal> as color palette definition.
+  pixmap <source.png> <palette.pixpal> [<options>] Transforms <source.png> into a pixel map 
+                          saving its color palette to <palette.pixpal>.
+
+Options:
+  --help,    -h            Displays this message.
+  --output,  -o            Specifies the name of the output file (by default, it is named after
+                           the source file).
+  --version, -v            Displays the version of the application.
+""" % [pkgTitle, pkgVersion, pkgDescription, pkgAuthor]
+
+proc error(code: int, msg: string) =
+  stderr.writeLine("(!) " & msg)
+  quit(code)
+
+proc info(msg: string) =
+  echo msg
+
 when isMainModule:
-  pixmap("example/test.in.png").write("example/test.pxmap", "example/test.pxplt")
-  png("example/test.pxmap", "example/test.pxplt").write("example/test.out.png")
+
+  var TARGET = ""
+  var OPT_OUTPUT = ""
+  var ARGS = newSeq[string]()
+
+
+
+  for kind, key, val in getopt():
+    case kind:
+      of cmdArgument:
+        ARGS.add key 
+      of cmdLongOption, cmdShortOption:
+        case key:
+          of "help", "h":
+            echo USAGE
+            quit(0)
+          of "version", "v":
+            echo pkgVersion
+            quit(0)
+          of "output", "o":
+            OPT_OUTPUT = val
+          else:
+            discard
+      else:
+        discard
+  
+  if ARGS.len == 0:
+    error(1, "No argument specified.")
+  if ARGS.len == 1:
+    error(2, "Palette file not specified.")
+  if ARGS.len > 2:
+    error(3, "Too many argyuments.")
+  
+  let SOURCE = ARGS[0].absolutePath
+  let PALETTE = ARGS[1].absolutePath
+
+  if not SOURCE.fileExists:
+    error(4, "Source file '$#' does not exist." % [SOURCE])
+  if not PALETTE.fileExists:
+    error(5, "Palette file '$#' does not exist." % [PALETTE])
+
+
+  if SOURCE.endsWith(".png") or SOURCE.endsWith(".PNG"):
+    TARGET = "pixmap"
+  else:
+    TARGET = "png"
+
+  if OPT_OUTPUT == "":
+    OPT_OUTPUT = Path(SOURCE).changeFileExt(TARGET).string
+
+  info("Generating: $#" % [OPT_OUTPUT])
+  if TARGET == "pixmap":
+    pixmap(SOURCE).write(TARGET, PALETTE)
+  elif TARGET == "png":
+    png(SOURCE, PALETTE).write(TARGET)
---
+++ src/pixmappkg/config.nim
@@ -0,0 +1,5 @@
+const
+  pkgTitle*       = "pixmap"
+  pkgVersion*     = "0.1.0"
+  pkgAuthor*      = "Fabio Cevasco"
+  pkgDescription* = "A small utility to convert simple PNG images to textual pixel maps and vice-versa."
